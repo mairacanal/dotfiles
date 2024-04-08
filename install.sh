ab754a28d2b7dfedd46f1cba1a55104c638e7b97
Maíra Canal <mairacanal@riseup.net>
2024-04-08 06:42:51 -0300

    install: partial update to the install script
    Actually, I do really want to move to Nix

--- install.sh
+++ install.sh
@@ -11,50 +11,41 @@ echo "Installing flatpak"
 echo "Done!"
 
 echo "Installing rpm packages"
-    sudo dnf install liberation-fonts
-
     wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
     sudo rpm -ivh appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
     sudo rm appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
-
 echo "Done!"
 
 echo "Installing dnf packages"
     sudo dnf install zathura-pdf-poppler xournalpp ripgrep python \
-                     nodejs zsh fzf clang clang-tools-extra powerline-fonts \
+                     nodejs zsh fzf clang clang-tools-extra \
                      fira-code-fonts gnome-tweaks gnome-shell-extension-pop-shell \
                      htop util-linux-user lld thunderbird xprop \
-                     gimp gnome-pomodoro alacritty
+                     gimp gnome-pomodoro alacritty neovim ripgrep tmux
 echo "Done!"
 
 echo "Installing flatpak packages"
     sudo flatpak install flathub com.spotify.Client
-    sudo flatpak install flathub com.discordapp.Discord
-    sudo flatpak install flathub im.riot.Riot
-    sudo flatpak install flathub com.visualstudio.code
 echo "Done!"
 
-echo "Installing vim-plug"
-    curl -fLo ~/dotfiles/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "Done!"
-
-echo "Setting vim in the system"
-    mkdir temp_dirs
-    cp .vimrc ~/
+echo "Setting nvim in the system"
+    cp ./config/nvim ~/.config
 echo "Done!"
 
 echo "Setting zsh in the system"
     # Installing oh-my-zsh
     sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-    # Installing spaceship theme
-    git clone https://github.com/moarram/headline.git $ZSH_CUSTOM/themes/headline
-    ln -s $ZSH_CUSTOM/themes/headline/headline.zsh-theme $ZSH_CUSTOM/themes/headline.zsh-theme
+    # Installing starship theme
+    curl -sS https://starship.rs/install.sh | sh
 
     # Installing zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+echo "Done!"
+
+echo "Setting tmux in the system"
+    cp .tmux.conf ~/
 echo "Done!"
 
 echo "Setting Alacritty in the system"
